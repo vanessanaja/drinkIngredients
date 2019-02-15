c39927b19d8da7e3de2e6e4c9b716ec19ebb2d6c
vanessanaja <vanessacnaja@gmail.com>
2019-02-15 13:36:09 -0800

    add drinks Schema

--- app.js
+++ app.js
@@ -6,10 +6,36 @@ const express = require('express'),
       mongoose = require('mongoose');
       require('dotenv').config();
 
+mongoose.connect('mongodb://localhost:27017/drinks', {useNewUrlParser: true});    
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride('_method'));
+
+const drinksSchema = new mongoose.Schema({
+  strDrink: String,
+  strCategory: String,
+  strDrinkThumb: String,
+  strGlass: String,
+  strIngredient1: String,
+  strIngredient2: String,
+  strIngredient3: String,
+  strIngredient4: String,
+  strIngredient5: String,
+  strIngredient6: String,
+  strIngredient7: String,
+  strIngredient8: String,
+  strIngredient9: String,
+  strInstructions: String,
+  strMeasure1: String,
+  strMeasure2: String,
+  strMeasure3: String,
+  strMeasure4: String,
+  strMeasure5: String,
+  strMeasure6: String,
+  strMeasure7: String,
+  strMeasure8: String
+});
 
 app.get('/', function(req, res){
   res.render('index');
